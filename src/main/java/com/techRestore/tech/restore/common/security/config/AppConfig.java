4afc57aacfa0726df8a783c7dd7b00e6c6563da3
Mokhtar <mostafa51moktar@gmail.com>
2025-09-30 19:36:47 +0300

    update: response of product

--- src/main/java/com/techRestore/tech/restore/common/security/config/AppConfig.java
+++ src/main/java/com/techRestore/tech/restore/common/security/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.techRestore.tech.restore.common.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.expression.spel.ast.Assign;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
--- src/main/java/com/techRestore/tech/restore/shop/dto/product/ProductResponseDTO.java
+++ src/main/java/com/techRestore/tech/restore/shop/dto/product/ProductResponseDTO.java
@@ -7,13 +7,14 @@ import java.util.UUID;
 import com.techRestore.tech.restore.common.model.enums.ProductCondition;
 
 public record ProductResponseDTO(
-        UUID id,
-        String name,
-        String description,
-        BigDecimal price,
-        Integer stock,
-        String imageUrl,
-        ProductCondition condition,
-        LocalDateTime createdAt,
-        String categoryName) {
+                UUID id,
+                String name,
+                String description,
+                BigDecimal price,
+                Integer stock,
+                String imageUrl,
+                ProductCondition condition,
+                LocalDateTime createdAt,
+                String categoryName,
+                boolean deleted) {
 }
